b922142dcc7ef720fa2cd3fea6337009d737a8b7
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-08 21:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ let counter = 0;
+
+        // Function to increment the counter
+        function incrementCounter() {
+            // Display the current value of counter
+            alert("Current Counter Value: " + counter);
+            // Increment the counter
+            counter++;
+            // Update the counter paragraph on the webpage
+            document.getElementById("counter").innerText = counter;
+        }
+
+        // Attach the incrementCounter function to the button click event
+        document.getElementById("incrementBtn").addEventListener("click", incrementCounter);
